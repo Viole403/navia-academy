a680ce55afae414db99d18ba323be7b762528d2c
Viole403 <m.maulana.fb26@gmail.com>
2022-11-04 09:00:00 +0700

    feat(backend): JWT utility package
    - `apps/backend/pkg/jwt/jwt.go`: provides JWT creation, parsing, and validation utilities for authentication.

---
+++ apps/backend/pkg/jwt/jwt.go
@@ -0,0 +1,125 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	Email  string `json:"email"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+type TokenPair struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn    int64  `json:"expires_in"`
+	TokenType    string `json:"token_type"`
+}
+
+type JWTService struct {
+	accessSecret     string
+	refreshSecret    string
+	accessDuration   time.Duration
+	refreshDuration  time.Duration
+	issuer           string
+}
+
+func New(accessSecret, refreshSecret string, accessDuration, refreshDuration time.Duration, issuer string) *JWTService {
+	return &JWTService{
+		accessSecret:    accessSecret,
+		refreshSecret:   refreshSecret,
+		accessDuration:  accessDuration,
+		refreshDuration: refreshDuration,
+		issuer:          issuer,
+	}
+}
+
+func (s *JWTService) GenerateTokenPair(userID, email, role string) (*TokenPair, error) {
+	now := time.Now()
+
+	accessClaims := Claims{
+		UserID: userID,
+		Email:  email,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(now.Add(s.accessDuration)),
+			IssuedAt:  jwt.NewNumericDate(now),
+			NotBefore: jwt.NewNumericDate(now),
+			Issuer:    s.issuer,
+			ID:        uuid.New().String(),
+		},
+	}
+
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, accessClaims)
+	accessStr, err := accessToken.SignedString([]byte(s.accessSecret))
+	if err != nil {
+		return nil, err
+	}
+
+	refreshClaims := Claims{
+		UserID: userID,
+		Email:  email,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(now.Add(s.refreshDuration)),
+			IssuedAt:  jwt.NewNumericDate(now),
+			NotBefore: jwt.NewNumericDate(now),
+			Issuer:    s.issuer,
+			ID:        uuid.New().String(),
+		},
+	}
+
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims)
+	refreshStr, err := refreshToken.SignedString([]byte(s.refreshSecret))
+	if err != nil {
+		return nil, err
+	}
+
+	return &TokenPair{
+		AccessToken:  accessStr,
+		RefreshToken: refreshStr,
+		ExpiresIn:    int64(s.accessDuration.Seconds()),
+		TokenType:    "Bearer",
+	}, nil
+}
+
+func (s *JWTService) ValidateAccessToken(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(s.accessSecret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, jwt.ErrSignatureInvalid
+	}
+
+	return claims, nil
+}
+
+func (s *JWTService) ValidateRefreshToken(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(s.refreshSecret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, jwt.ErrSignatureInvalid
+	}
+
+	return claims, nil
+}
+
+func (s *JWTService) GetAccessSecret() string {
+	return s.accessSecret
+}
