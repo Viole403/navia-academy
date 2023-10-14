3ca480a7495b18046e2f842e243e3527da20ecd9
Viole403 <m.maulana.fb26@gmail.com>
2023-10-14 09:00:00 +0700

    feat(web): app layout, globals CSS, error & not-found
    - `apps/web/src/components/ui/animated-grid-pattern.tsx`: adds animated grid background pattern component with configurable cell size and animation timing.
    - `apps/web/src/components/ui/scroll-based-velocity.tsx`: adds component that applies horizontal motion based on scroll velocity and direction.
    - `apps/web/src/components/ui/index.ts`: adds barrel export file centralizing public exports for UI components.

---
+++ apps/web/src/app/error.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Link from "next/link";
+import { TriangleAlert } from "lucide-react";
+import { Button } from "@/components/ui";
+import { useAuth, roleHomePath } from "@/lib/auth-context";
+import { useTranslation } from "@/i18n/locale-context";
+
+export default function ErrorPage({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  const { t } = useTranslation();
+  const { user } = useAuth();
+  const ctaLabel = user?.role === "admin" ? t("notFound.admin") : user?.role === "contributor" || user?.role === "reviewer" ? t("notFound.contributor") : t("nav.dashboard");
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-bg px-4 text-center">
+      <TriangleAlert className="h-16 w-16 text-warn" aria-hidden />
+      <h1 className="font-display mt-6 text-3xl font-bold">{t("error.title")}</h1>
+      <p className="mt-2 max-w-sm text-ink-soft">
+        {t("error.description")}
+      </p>
+      {error.digest && <p className="mt-2 font-mono text-xs text-ink-faint">Ref: {error.digest}</p>}
+      <div className="mt-8 flex gap-3">
+        <Button onClick={reset}>{t("common.tryAgain")}</Button>
+        <Link href="/">
+          <Button variant="outline">{t("notFound.home")}</Button>
+        </Link>
+        {user ? (
+          <Link href={roleHomePath(user.role)}>
+            <Button variant="outline">{ctaLabel}</Button>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <Button variant="outline">{t("auth.login")}</Button>
+          </Link>
+        )}
+      </div>
+    </main>
+  );
+}
---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { MapPinOff } from "lucide-react";
+import { Button } from "@/components/ui";
+import { useAuth, roleHomePath } from "@/lib/auth-context";
+import { useTranslation } from "@/i18n/locale-context";
+
+export default function NotFound() {
+  const { user, loading } = useAuth();
+  const { t } = useTranslation();
+  const ctaLabel = user?.role === "admin" ? t("notFound.admin") : user?.role === "contributor" || user?.role === "reviewer" ? t("notFound.contributor") : t("nav.dashboard");
+
+  return (
+    <main className="paper-texture flex min-h-screen flex-col items-center justify-center bg-bg px-4 text-center">
+      <MapPinOff className="h-16 w-16 text-ink-faint" aria-hidden />
+      <h1 className="font-display mt-6 text-4xl font-bold">404 · {t("error.notFound")}</h1>
+      <p className="mt-2 max-w-sm text-ink-soft">{t("error.notFoundDesc")}</p>
+      <div className="mt-8 flex gap-3">
+        <Link href="/">
+          <Button variant="outline">{t("notFound.home")}</Button>
+        </Link>
+        {loading ? null : user ? (
+          <Link href={roleHomePath(user.role)}>
+            <Button>{ctaLabel}</Button>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <Button>{t("auth.login")}</Button>
+          </Link>
+        )}
+      </div>
+    </main>
+  );
+}
