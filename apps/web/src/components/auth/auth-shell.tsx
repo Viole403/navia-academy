e58089a201f4ced568b010a9548e33f55214dc93
Viole403 <m.maulana.fb26@gmail.com>
2023-11-18 09:00:00 +0700

    feat(web): auth shell component
    - `apps/web/src/components/auth/auth-shell.tsx`: provides a reusable layout shell for authentication pages.

---
+++ apps/web/src/components/auth/auth-shell.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import type { ReactNode } from "react";
+import { Logo } from "@/components/ui/logo";
+
+export function AuthShell({ title, subtitle, children }: { title: string; subtitle: string; children: ReactNode }) {
+  return (
+    <main className="paper-texture flex min-h-screen items-center justify-center bg-bg px-4 py-10">
+      <div className="w-full max-w-md">
+        <Link href="/" className="mb-8 flex items-center justify-center gap-2.5" aria-label="Back to homepage">
+          <Logo className="h-10 w-10" />
+          <span className="font-display text-xl font-bold">Navia</span>
+        </Link>
+        <div className="rounded-[var(--radius)] border border-line bg-raised p-6 shadow-[var(--shadow)] sm:p-8">
+          <h1 className="font-display text-2xl font-bold text-ink">{title}</h1>
+          <p className="mt-1 text-sm text-ink-faint">{subtitle}</p>
+          <div className="mt-6">{children}</div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export function GoogleIcon() {
+  return (
+    <svg viewBox="0 0 24 24" className="h-4 w-4" aria-hidden>
+      <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92a5.06 5.06 0 0 1-2.2 3.32v2.77h3.57c2.08-1.92 3.27-4.74 3.27-8.1z" />
+      <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84A11 11 0 0 0 12 23z" />
+      <path fill="#FBBC05" d="M5.84 14.1a6.6 6.6 0 0 1 0-4.2V7.06H2.18a11 11 0 0 0 0 9.88l3.66-2.84z" />
+      <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15A11 11 0 0 0 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z" />
+    </svg>
+  );
+}
