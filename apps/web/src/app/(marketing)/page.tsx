9c706f6a4072c458b36910fcfa784e12cbebcf60
Viole403 <m.maulana.fb26@gmail.com>
2023-12-11 09:00:00 +0700

    feat(web): marketing layout + landing skeleton
    - `apps/web/src/app/(marketing)/layout.tsx`: provides the layout wrapper for the marketing section, including shared elements.
    - `apps/web/src/app/(marketing)/page.tsx`: creates the landing page skeleton structure with placeholder content sections.

---
+++ apps/web/src/app/(marketing)/page.tsx
@@ -0,0 +1,19 @@
+import { landingDemo } from "@/lib/landing-demo.generated";
+import type { LandingInitial } from "@/lib/data-client";
+import { LandingPage } from "@/components/marketing/landing-page";
+
+export default async function MarketingHomePage() {
+  // Baked in at publish time by apps/media (scripts/publish-data.ts →
+  // writeLandingDemoStatic). No CDN fetch, no manifest lookup, no network
+  // race/timeout — the landing page's first paint never depends on
+  // storage/CDN reachability.
+  const initial: LandingInitial = {
+    examConfig: landingDemo.exams,
+    vocab: landingDemo.preview?.zh ?? [],
+    stats: landingDemo.stats ?? {},
+    placementTotal: landingDemo.placementTotal,
+    audio: landingDemo.audio,
+    preview: landingDemo.preview,
+  };
+  return <LandingPage initial={initial} />;
+}
